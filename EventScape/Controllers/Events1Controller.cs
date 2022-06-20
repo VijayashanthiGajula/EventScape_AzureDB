99e8d6327a5337627a09725d081a7ba2c0c029eb
VijayaShanthi Gajula <vijayashanthi.gajula@gmail.com>
2022-06-20 22:57:24 +1200

    API part

---
+++ EventScape/Controllers/Events1Controller.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventScape.Data;
+using EventScape.Models;
+
+namespace EventScape.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Events1Controller : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Events1Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Events1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Events>>> GetEvents()
+        {
+          if (_context.Events == null)
+          {
+              return NotFound();
+          }
+            return await _context.Events.ToListAsync();
+        }
+
+        // GET: api/Events1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Events>> GetEvents(int id)
+        {
+          if (_context.Events == null)
+          {
+              return NotFound();
+          }
+            var events = await _context.Events.FindAsync(id);
+
+            if (events == null)
+            {
+                return NotFound();
+            }
+
+            return events;
+        }
+
+        // PUT: api/Events1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvents(int id, Events events)
+        {
+            if (id != events.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(events).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Events1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Events>> PostEvents(Events events)
+        {
+          if (_context.Events == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Events'  is null.");
+          }
+            _context.Events.Add(events);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEvents", new { id = events.ID }, events);
+        }
+
+        // DELETE: api/Events1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvents(int id)
+        {
+            if (_context.Events == null)
+            {
+                return NotFound();
+            }
+            var events = await _context.Events.FindAsync(id);
+            if (events == null)
+            {
+                return NotFound();
+            }
+
+            _context.Events.Remove(events);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EventsExists(int id)
+        {
+            return (_context.Events?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
